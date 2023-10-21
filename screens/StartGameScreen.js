0c8ec1683dbe842e520d53c0ececc65c543409ae
lee gyuseung <gyus0421@gmail.com>
2023-10-21 22:49:34 +0900

    Minigame : ios, android에 따른 스타일링

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,15 +1,32 @@
-import { TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 // 시작하는 화면
 function StartGameScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    // android only 그림자
+    elevation: 4,
+    // ios 그림자
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
 
 export default StartGameScreen;
