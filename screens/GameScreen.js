9380ed3597d18b13f9fb026bc095e97a6cf74459
lee gyuseung <gyus0421@gmail.com>
2023-11-05 23:20:05 +0900

    Minigame : 게임화면 작업 시작

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,8 +1,25 @@
-import { Text } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 // 실질적으로 게임을 담당하는 화면
 function GameScreen() {
-  return <Text>Game Screen!</Text>;
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent`s Guess</Text>
+      {/* GUESS */}
+      <View>
+        <Text>Higher or Lower?</Text>
+        {/* + - */}
+      </View>
+      <View>{/* Log Rounds */}</View>
+    </View>
+  );
 }
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
+  },
+});
