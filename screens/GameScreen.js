7d79e5adc28f7258b575f7c193f7196d736dc8ac
lee gyuseung <gyus0421@gmail.com>
2023-11-06 23:04:05 +0900

    Minigame : 제목 컴포넌트 생성

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,11 @@
 import { Text, View, StyleSheet } from "react-native";
+import Title from "../components/Title";
 
 // 실질적으로 게임을 담당하는 화면
 function GameScreen() {
   return (
     <View style={styles.screen}>
-      <Text>Opponent`s Guess</Text>
+      <Title>Opponent`s Guess</Title>
       {/* GUESS */}
       <View>
         <Text>Higher or Lower?</Text>
@@ -21,5 +22,14 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 24,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#4e0329",
+    textAlign: "center",
+    borderWidth: 2,
+    borderColor: "#4e0329",
+    padding: 12,
   },
 });
