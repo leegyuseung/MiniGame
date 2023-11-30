81e11059934c7952c67c3be98ae8bd9ddc819505
lee gyuseung <gyus0421@gmail.com>
2023-12-01 02:42:40 +0900

    Minigame : 상태바 스타일링

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { LinearGradient } from "expo-linear-gradient";
 // import { useFonts } from "expo-font";
 // import AppLoading from "expo-app-loading";
-
+import { StatusBar } from "expo-status-bar";
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
 import GameOverScreen from "./screens/GameOverScreen";
@@ -57,19 +57,23 @@ export default function App() {
   }
 
   return (
-    <LinearGradient
-      colors={[Colors.accent500, Colors.primary800]}
-      style={styles.rootScreen}
-    >
-      <ImageBackground
-        source={require("./assets/images/background.png")}
-        resizeMode="cover"
+    <>
+      {/* black, light */}
+      <StatusBar style="light" />
+      <LinearGradient
+        colors={[Colors.accent500, Colors.primary800]}
         style={styles.rootScreen}
-        imageStyle={styles.backgroundImage}
       >
-        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
-      </ImageBackground>
-    </LinearGradient>
+        <ImageBackground
+          source={require("./assets/images/background.png")}
+          resizeMode="cover"
+          style={styles.rootScreen}
+          imageStyle={styles.backgroundImage}
+        >
+          <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
+        </ImageBackground>
+      </LinearGradient>
+    </>
   );
 }
 
